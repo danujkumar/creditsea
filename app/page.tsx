a9baf37cb752f57485e9563c503383694c896b22
D Anuj Kumar <anuj.as828@gmail.com>
2025-03-26 22:44:19 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+
+import NavbarComponent from "@/components/Navbar";
+import Middle from '@/components/Middle'
+
+export default function Home() {
+  return (
+    <div>
+      {/* <NavbarComponent/> */}
+      <Middle/>
+    </div>
+  );
+}
